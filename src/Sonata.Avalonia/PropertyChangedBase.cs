923085cd17683b70d821ad7aa70ef7509ccb35be
Olivier <oburri.developments@gmail.com>
2026-08-25 21:25:34 +0200

    docs(core): restore XML documentation on PropertyChangedBase protected members

--- src/Sonata.Avalonia/PropertyChangedBase.cs
+++ src/Sonata.Avalonia/PropertyChangedBase.cs
@@ -21,6 +21,8 @@ public abstract class PropertyChangedBase : INotifyPropertyChanged
     /// <summary>
     /// Raise a PropertyChanged notification from the property in the given expression, e.g. NotifyOfPropertyChange(() => this.Property)
     /// </summary>
+    /// <typeparam name="TProperty">Type of property being notified</typeparam>
+    /// <param name="property">Expression describing the property to raise a PropertyChanged notification for</param>
     protected virtual void NotifyOfPropertyChange<TProperty>(Expression<Func<TProperty>> property)
     {
         OnPropertyChanged(property.NameForProperty());
@@ -29,6 +31,7 @@ public abstract class PropertyChangedBase : INotifyPropertyChanged
     /// <summary>
     /// Raise a PropertyChanged notification from the property with the given name
     /// </summary>
+    /// <param name="propertyName">Name of the property to raise a PropertyChanged notification for. Defaults to the calling property</param>
     protected virtual void NotifyOfPropertyChange([CallerMemberName] string propertyName = "")
     {
         OnPropertyChanged(propertyName);
@@ -38,6 +41,7 @@ public abstract class PropertyChangedBase : INotifyPropertyChanged
     /// Fires the PropertyChanged notification on the UI thread (non-blocking).
     /// </summary>
     /// <remarks>Specially named so that Fody.PropertyChanged calls it</remarks>
+    /// <param name="propertyName">Name of the property to raise the notification for</param>
     [EditorBrowsable(EditorBrowsableState.Never)]
     protected virtual void OnPropertyChanged(string propertyName)
     {
@@ -49,6 +53,11 @@ public abstract class PropertyChangedBase : INotifyPropertyChanged
     /// <summary>
     /// Takes, by reference, a field, and its new value. If field != value, will set field = value and raise a PropertyChanged notification
     /// </summary>
+    /// <typeparam name="T">Type of field being set and notified</typeparam>
+    /// <param name="field">Field to assign</param>
+    /// <param name="value">Value to assign to the field, if it differs</param>
+    /// <param name="propertyName">Name of the property to notify for. Defaults to the calling property</param>
+    /// <returns>True if field != value and a notification was raised; false otherwise</returns>
     protected virtual bool SetAndNotify<T>(ref T field, T value, [CallerMemberName] string propertyName = "")
     {
         if (!EqualityComparer<T>.Default.Equals(field, value))
